7e0439968b027295a820efabb4b967d05aa93206
Gianluigi De Marco <dem.gianluigi@gmail.com>
2023-01-17 17:05:09 +0100

    initial commit

---
+++ src/test/java/dev/dmgiangi/domobotserver/DomobotServerApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.dmgiangi.domobotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomobotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+
+}
